ee15abbb0dcd74331d79cc216de791a63ec1e0d9
Denis <apricot103@mail.ru>
2023-07-10 01:18:55 +0300

    add mail service

---
+++ mail-service/src/main/java/com/exmple/mail/controller/MailController.java
@@ -0,0 +1,23 @@
+package com.exmple.mail.controller;
+
+import com.exmple.mail.dto.request.SendMessageByTimeRequest;
+import com.exmple.mail.service.MailService;
+import java.util.Date;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/mails")
+@RequiredArgsConstructor
+public class MailController {
+    private final MailService mailService;
+
+    @PostMapping("/by-time/send")
+    public void sendMessage(@RequestBody @Valid SendMessageByTimeRequest request) {
+        mailService.sendMessage(request.getRecipient(), request.getText(), request.getTitle());
+    }
+}
---
+++ mail-service/src/main/java/com/exmple/mail/dto/request/SendMessageByTimeRequest.java
@@ -0,0 +1,15 @@
+package com.exmple.mail.dto.request;
+
+import javax.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SendMessageByTimeRequest {
+    @Pattern(message = "Recipient is not valid", regexp =
+            "^\\S+@\\S+\\.\\S+$")
+    private String recipient;
+    private String title;
+    private String text;
+}
